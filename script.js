4fbf39467904242509103ec597e4766c2b369839
Rocky <55082473+iRocky1337@users.noreply.github.com>
2024-12-20 16:56:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+START PROGRAM
+Skriv in totala notan (endast positiva tal):
+Läs in totalNotan
+
+Om totalNotan < 0 DÅ
+Skriv ut "Notan kan inte vara negativ."
+Avsluta programmet
+
+Skriv in hur många ska dela på notan (minst 1 person):
+Läs in antalVanner
+
+Om antalVanner <= 0 DÅ
+Skriv ut "Måste vara minst en person som delar på notan."
+Avsluta programmet
+
+Skriv in dricks som en procent (t.ex. 10 för 10%):
+Läs in dricksProcent
+
+Beräkna dricks: dricks = totalNotan * (dricksProcent / 100)
+Lägg till dricksen på den totala notan: totalMedDricks = totalNotan + dricks
+
+Dela notan inklusive dricks mellan alla vänner: beloppPerPerson = totalMedDricks / antalVanner
+Skriv ut "Varje person betalar: " + beloppPerPerson + " kr"
+SLUT PROGRAM
+*/
+
+
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
